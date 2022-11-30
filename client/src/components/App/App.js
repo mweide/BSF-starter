b29703f2e061d42fba9badcf726ef9422a7582be
mweide <ms22@gomediapro.com>
2022-11-29 21:14:23 -0700

    Might have made a thing

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -3,18 +3,13 @@ import './App.css';
 import AddEntry from '../AddEntry.jsx';
 import CurrentEntries from '../CurrentEntries.jsx';
 import Footer from '../Footer.jsx';
+import Search from '../Search.jsx';
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Entries</h1>
-
-      <AddEntry />
-      <hr />
-      <CurrentEntries />
-      <hr />
-      <Footer />
+      <Search />
     </div>
   )
 }
